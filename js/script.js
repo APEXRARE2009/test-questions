d06f77b3e188b8237ea6b6abc2b60e78bb19e290
2009 <83235862+APEXRARE2009@users.noreply.github.com>
2024-04-28 14:28:52 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,33 @@ let attemptNum = document.querySelector(".hero__content-block-before-attempt-spa
 let questionTxt = document.querySelector(".hero__content-block-after-question");
 let answersBlock = document.querySelector(".hero__content-block-after-answers");
 
+let blockCheckDevice = document.querySelector(".checking-device");
+let blockCheckDeviceContent = document.querySelector(".checking-device-content");
+let blockCheckDeviceBtn = document.querySelector(".checking-device-content-btn");
+let blockCheckDeviceTxt = document.querySelector(".checking-device-content-txt");
+
+let agentInfo = navigator.userAgent;
+
+if (agentInfo.includes("Android")) {
+    blockCheckDevice.style.display = "flex";
+} else if (agentInfo.includes("iPhone")) {
+    blockCheckDevice.style.display = "flex"
+}
+
+blockCheckDeviceBtn.addEventListener("click", () => {
+    setTimeout(() => {
+        blockCheckDevice.style.display = "none";
+    }, 2300);
+    setTimeout(() => {
+        blockCheckDevice.classList.add("checking-device-close")
+    }, 1200);
+    setTimeout(() => {
+        blockCheckDeviceContent.classList.add("checking-device-content-close")
+    }, 400);
+    blockCheckDeviceBtn.classList.add("checking-device-content-btn-close")
+    blockCheckDeviceTxt.classList.add("checking-device-content-txt-close")
+})
+
 let questions = [
 	{
 		question: "Какой язык програмирования используеться для написания frontend?",
